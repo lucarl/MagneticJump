7a8a5388e50f446959ed34435d0370094ef1a075
lucarl <carl.e.w.lundborg@gmail.com>
2020-11-16 11:51:12 +0100

    adding movement to character

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private bool jumpKeyWasPressed;
+    private bool isGrounded;
+    private float horizontalInput;
+    private Rigidbody rigidBodyComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+    // Called every physics update    
+    private void FixedUpdate()
+    {
+        if (!isGrounded)
+        {
+            return;
+        }
+        
+        if (jumpKeyWasPressed)
+        {
+            rigidBodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+        
+        rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+}
